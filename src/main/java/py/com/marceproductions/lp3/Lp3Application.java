c26b9d804a936ee83ae588726c5e71bc15f5f788
Néstor Valdez <ness_valdez@hotmail.com>
2018-12-02 19:59:49 -0300

    Initial commit

---
+++ src/main/java/py/com/marceproductions/lp3/Lp3Application.java
@@ -0,0 +1,12 @@
+package py.com.marceproductions.lp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp3Application.class, args);
+	}
+}
